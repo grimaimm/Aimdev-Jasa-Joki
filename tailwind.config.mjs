8abbf7b4945764f45cf3243ca766ba4a785f567b
grimaim <muhammadrahim.mr196@gmail.com>
2025-01-30 18:47:25 +0700

    chore (update): file configuration

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,17 +1,51 @@
 /** @type {import('tailwindcss').Config} */
+
+const { heroui } = require('@heroui/react');
+
 export default {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    // Next.js
+    './src/**/*.{js,ts,jsx,mdx}',
+    './src/**/**/*.{js,ts,jsx,mdx}',
+    './src/**/**/**/*.{js,ts,jsx,mdx}',
+    './src/**/**/**/**/*.{js,ts,jsx,mdx}',
+    './src/**/**/**/**/**/*.{js,ts,jsx,mdx}',
+    './src/**/**/**/**/**/**/*.{js,ts,jsx,mdx}',
+    './src/**/**/**/**/**/**/**/*.{js,ts,jsx,mdx}',
+    './src/**/**/**/**/**/**/**/**/*.{js,ts,jsx,mdx}',
+    './src/**/**/**/**/**/**/**/**/**/*.{js,ts,jsx,mdx}',
+
+    // HeroUI
+    './node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
+    extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('daisyui'),
+    heroui({
+      prefix: 'heroui',
+      addCommonColors: false,
+      defaultTheme: 'light',
+      themes: {
+        light: {
+          colors: {},
+        },
+        dark: {
+          colors: {},
+        },
+      },
+    }),
+  ],
+
+  // daisyUI config (optional - here are the default values)
+  daisyui: {
+    themes: false,
+    base: true,
+    styled: true,
+    utils: true,
+    prefix: 'daisyui',
+    logs: true,
+    themeRoot: ':root',
+  },
 };
