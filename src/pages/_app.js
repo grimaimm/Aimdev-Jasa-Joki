c7a3f8dfc4f188d792270618f27f3497bf16a02f
grimaim <muhammadrahim.mr196@gmail.com>
2025-02-05 18:38:37 +0700

    chore (add & update): src pages

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,13 +2,20 @@ import * as React from 'react';
 import '@/styles/globals.css';
 import { HeroUIProvider } from '@heroui/system';
 import { ThemeProvider as NextThemesProvider } from 'next-themes';
+import { AppLayouts } from '@/common/components/layouts/AppLayouts';
+import StyleGlobal from '@/common/components/elements/StyleGlobal';
 
 export default function App({ Component, pageProps }) {
   return (
-    <HeroUIProvider>
-      <NextThemesProvider attribute='class' defaultTheme='dark'>
-        <Component {...pageProps} />
-      </NextThemesProvider>
-    </HeroUIProvider>
+    <>
+      <StyleGlobal />
+      <HeroUIProvider>
+        <NextThemesProvider attribute='class' defaultTheme='light'>
+          <AppLayouts>
+            <Component {...pageProps} />
+          </AppLayouts>
+        </NextThemesProvider>
+      </HeroUIProvider>
+    </>
   );
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,20 @@
-import dynamic from 'next/dynamic';
+// import LandingPage from '@/modules/LandingPage/LandingPage';
+import LandingPage from '@/modules/LandingPage';
+import { NextSeo } from 'next-seo';
+// import dynamic from 'next/dynamic';
 import React from 'react';
 
-const ThemeSwitcher = dynamic(
-  () => import('@/common/components/elements/ThemeSwitcher'),
-  { ssr: false },
-);
+// const ThemeSwitcher = dynamic(
+//   () => import('@/common/components/elements/ThemeSwitcher'),
+//   { ssr: false },
+// );
 
 export default function Home() {
-  return <ThemeSwitcher />;
+  return (
+    <>
+      <NextSeo title='Home | AimdevJT' />
+      {/* <LandingPage /> */}
+      <LandingPage />
+    </>
+  );
 }
