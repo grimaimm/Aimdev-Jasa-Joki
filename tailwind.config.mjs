4af9794d1edddb7696f9431fdb8ecd47907fb46a
grimaim <muhammadrahim.mr196@gmail.com>
2025-02-05 18:51:19 +0700

    chore (update): config and package

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -41,21 +41,12 @@ export default {
   ],
 
   // daisyUI config (optional - here are the default values)
-  // daisyui: {
-  //   themes: false,
-  //   base: true,
-  //   styled: true,
-  //   utils: true,
-  //   logs: true,
-  //   themeRoot: ':root',
-  // },
   daisyui: {
-    themes: false, // false: only light + dark | true: all themes | array: specific themes like this ["light", "dark", "cupcake"]
-    darkTheme: "dark", // name of one of the included themes for dark mode
-    base: true, // applies background color and foreground color for root element by default
-    styled: true, // include daisyUI colors and design decisions for all components
-    utils: true, // adds responsive and modifier utility classes
-    logs: true, // Shows info about daisyUI version and used config in the console when building your CSS
-    themeRoot: ":root", // The element that receives theme color CSS variables
+    themes: false,
+    base: true,
+    styled: true,
+    utils: true,
+    logs: true,
+    themeRoot: ':root',
   },
 };
