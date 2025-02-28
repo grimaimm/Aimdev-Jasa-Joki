27d1279ab6b347c1ba8886c4da6edc5a4470e66a
grimaim <muhammadrahim.mr196@gmail.com>
2025-02-28 19:33:33 +0700

    update ButtonCustom.jsx

--- src/common/components/elements/ButtonCustom.jsx
+++ src/common/components/elements/ButtonCustom.jsx
@@ -7,14 +7,11 @@ export const ButtonPrimary = ({ title, className = '', link }) => {
     <Button
       className={`btn border-none bg-gradient-to-tr from-teal-400/80 to-cyan-400/80 font-semibold shadow-lg dark:from-teal-600/80 dark:to-cyan-600/80 ${className}`}
       radius='full'
+      target='_blank'
+      as={Link}
+      href={link}
     >
-      {link ? (
-        <Link target='_blank' href={link}>
-          {title}
-        </Link>
-      ) : (
-        title
-      )}
+      {title}
     </Button>
   );
 };
@@ -24,14 +21,26 @@ export const ButtonSecondary = ({ title, link }) => {
     <Button
       className='btn border border-zinc-200 bg-zinc-100 font-semibold shadow-lg dark:border-zinc-800 dark:bg-zinc-900/80'
       radius='full'
+      target='_blank'
+      as={Link}
+      href={link}
     >
-      {link ? (
-        <Link target='_blank' href={link}>
-          {title}
-        </Link>
-      ) : (
-        title
-      )}
+      {title}
+    </Button>
+  );
+};
+
+export const ButtonTertiary = ({ title, link }) => {
+  return (
+    <Button
+      showAnchorIcon
+      as={Link}
+      href='https://github.com/heroui-inc/heroui'
+      className='btn border border-zinc-200 bg-zinc-100 font-semibold shadow-lg dark:border-zinc-800 dark:bg-zinc-900/80'
+      radius='full'
+      target='_blank'
+    >
+      Button Link
     </Button>
   );
 };
