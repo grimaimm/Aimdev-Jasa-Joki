33799cd7e798f466d94ccea9165ebcdd01174c23
grimaim <muhammadrahim.mr196@gmail.com>
2025-03-03 18:04:35 +0700

    update src/pages/index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,41 +1,15 @@
 import React from 'react';
 import Home from '@/modules/homePage';
-import { LocalBusinessJsonLd, NextSeo } from 'next-seo';
+import { NextSeo } from 'next-seo';
 
 export default function HomePage() {
   return (
     <>
-      <NextSeo title='Aimdev Joki Tugas Website' />
-      <Home />
-      <LocalBusinessJsonLd
-        type='Joki Tugas'
-        id='https://jokitugas.aimdev.web.id'
-        name='Aimdev Joki Tugas Website'
+      <NextSeo
+        title='Aimdev Joki Tugas Website'
         description='Kami yang buat, kamu fokus ke hal lain. Jadi, gak perlu pusing ngoding, kami yang urusin! Hehe3x.'
-        url='https://jokitugas.aimdev.web.id'
-        telephone='+6281325970587'
-        address={{
-          addressLocality: 'Yogyakarta',
-          addressRegion: 'Daerah Istimewa Yogyakarta',
-          addressCountry: 'ID',
-        }}
-        images={['https://jokitugas.aimdev.web.id/images/introduction_one.png']}
-        openingHours={[
-          {
-            opens: '00:00',
-            closes: '23:59',
-            dayOfWeek: [
-              'Monday',
-              'Tuesday',
-              'Wednesday',
-              'Thursday',
-              'Friday',
-              'Saturday',
-              'Sunday',
-            ],
-          },
-        ]}
       />
+      <Home />
     </>
   );
 }
